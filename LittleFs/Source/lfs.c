80b63017a0aeaa6600a7d3ebfe19ba838b9a5237
Wenshuai.Song <wenshuaisong@gmail.com>
2021-09-12 22:30:20 +0800

    del not used file.

--- LittleFs/Source/lfs.c
+++
@@ -1,8 +0,0 @@
-/*
- * The little filesystem
- *
- * Copyright (c) 2017, Arm Limited. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- */
-#include "lfs.h"
-#include "lfs_util.h"
--- LittleFs/Source/lfs_util.c
+++
@@ -1,8 +0,0 @@
-/*
- * lfs util functions
- *
- * Copyright (c) 2017, Arm Limited. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- */
-#include "lfs_util.h"
-
