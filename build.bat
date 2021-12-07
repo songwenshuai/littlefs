9ac601393c6dc0734bf2a44f8ddd73b4f7d03488
Wenshuai.Song <wenshuaisong@gmail.com>
2021-12-07 21:47:24 +0800

    delete cmake file.

--- build.bat
+++
@@ -1,2 +0,0 @@
-cmake -AWin32 -Bbuild -DCMAKE_TOOLCHAIN_FILE=./cmake/win32.cmake
-cmake --build ./build
--- build.sh
+++
@@ -1,4 +0,0 @@
-clear
-rm -rf build/
-cmake -Bbuild -DCMAKE_TOOLCHAIN_FILE=./cmake/linux.cmake -GNinja .
-cmake --build ./build
